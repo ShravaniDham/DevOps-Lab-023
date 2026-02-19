11a3e13f48edbfc38b79065c1ffe88965f934e01
ShravaniDham <156278367+ShravaniDham@users.noreply.github.com>
2026-02-19 12:10:57 +0530

    Add StudentInfo class to display student details

---
+++ StudentInfo.java
@@ -0,0 +1,16 @@
+public class StudentInfo {
+
+    public static void main(String[] args) {
+
+        String name = "Shravani";
+        int rollNo = 23;
+        String course = "Information Technology";
+
+        System.out.println("----- Student Information -----");
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Course: " + course);
+
+        System.out.println("\nBuild executed successfully using Jenkins CI/CD Pipeline!");
+    }
+}
